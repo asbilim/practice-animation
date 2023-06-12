89ebab49153bfb5f8d148922c99eb10d20cbdfc0
asbilim <bilimdepaulilian@gmail.com>
2023-06-12 12:06:01 +0100

    stopping for now

--- app/index.jsx
+++ app/index.jsx
@@ -1,32 +1,86 @@
-import { MotiText,MotiView,MotiScrollView ,MotiImage} from "moti";
+import { MotiText,MotiView ,MotiImage} from "moti";
+import { FlatList } from "react-native";
 import {COLORS} from "../constants/colors";
-import { Dimensions } from "react-native";
+import { Dimensions , Animated,ScrollView} from "react-native";
 import { MotiSafeAreaView } from "moti";
-import { Tabs } from "expo-router";
 import { SIZES } from "../constants/sizes";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import picture from "../assets/kemal.jpg"
+import chaka from "../assets/chaka.jpeg"
+import one from "../assets/bg2.jpg"
+import two from "../assets/bg1.jpg"
+import three from "../assets/bg3.jpg"
 import { StatusBar } from "expo-status-bar";
 import { EvilIcons } from '@expo/vector-icons';
 import { TextInput } from "react-native";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { ImageBackground } from "react-native";
+import {faker} from "@faker-js/faker"
+import { useMemo, useRef,useState } from "react";
+import { MotiPressable } from 'moti/interactions'
+
+const IMAGES = [
+    {
+      id: 1,
+      src: chaka,
+      name: faker.person.firstName(),
+    },
+    {
+      id: 2,
+      src: one,
+      name: faker.person.firstName(),
+    },
+    {
+      id: 3,
+      src: two,
+      name: faker.person.firstName(),
+    },
+    {
+      id: 4,
+      src: three,
+      name: faker.person.firstName(),
+    }
+  ];
+
+
 
 export default function Main(){
 
+    
     const {width, height} = Dimensions.get("screen")
     const {top} = useSafeAreaInsets(); 
-   
+    const position = useRef(new Animated.Value(0)).current
+    const [bg,setBg] = useState(chaka)
+
+    const Card = ({name,id,image})=>{
+        const [showText,setShowText] = useState(false)
+        return (
+            <MotiPressable 
+                onPress={()=>{
+                    setShowText(!showText)
+                    setBg(image)
+                }}
+                animate={{rotateZ: `${showText ?  0: 2}deg`,scale: parseInt(`${showText ? 1.3 : 1}`)}}
+                transition={{
+                    duration:200,
+                    type:"timing"
+                }}
+                key={id} style={{height:300,marginHorizontal:10}}   >
+                <MotiImage source={image} style={[{width: 220, height: 250,borderRadius:15}]}  resizeMode="cover" />
+                {
+                    showText && (<MotiText style={{fontSize:18,textAlign:"center",color:COLORS.lightGrey,fontWeight:800,marginVertical:15}} from={{opacity:0,translateY:500}} animate={{opacity:1,translateY:0}} transition={{duration:200,type:"spring"}}>{name}</MotiText>)
+                }
+            </MotiPressable>
+        )
+    }
+
+
+
     return (
         <MotiSafeAreaView style={{flex:1}}>
-            <StatusBar hidden />
-            <Tabs.Screen 
-                options={{
-                    headerShown:false,
-                }}
-            />
-            <ImageBackground source={picture} >
-                <MotiScrollView 
+            <StatusBar style={{backgroundColor:"transparent"}} />
+            <ImageBackground source={bg} resizeMode="cover" blurRadius={1.5} >
+                <ScrollView 
                     style={{backgroundColor:"transparent",minHeight:height}}
                 >
 
@@ -57,7 +111,7 @@ export default function Main(){
 
                     {/* search */}
 
-                    <MotiView style={{marginHorizontal:SIZES.extraLarge,marginVertical:SIZES.extraLarge,backgroundColor:"transparent",paddingVertical:SIZES.small,borderRadius:8,borderColor:COLORS.white,borderWidth:0.5,paddingHorizontal:SIZES.small,flexDirection:"row",justifyContent:"space-between"}} blurRadius={40}>
+                    <MotiView style={{marginHorizontal:SIZES.extraLarge,marginTop:SIZES.extraLarge,backgroundColor:"transparent",paddingVertical:SIZES.small,borderRadius:8,borderColor:COLORS.white,borderWidth:0.5,paddingHorizontal:SIZES.small,flexDirection:"row",justifyContent:"space-between"}} blurRadius={40}>
                         <MotiView
                             style={{flex:8,flexDirection:"row",alignItems:"center"}}
                         >
@@ -70,7 +124,48 @@ export default function Main(){
                             <MaterialCommunityIcons name="microphone-outline" size={24} color={COLORS.darkGrey} />
                         </MotiView>
                     </MotiView>
-                </MotiScrollView>
+
+                    {/* informations */}
+
+                    <MotiView style={{marginVertical:SIZES.extraLarge,backgroundColor:"transparent",paddingVertical:SIZES.small,paddingHorizontal:SIZES.small,flexDirection:"column",justifyContent:"space-between"}} blurRadius={40}>
+                        
+                        <MotiView style={{flexDirection:"row",justifyContent:"center",alignItems:"center",padding:SIZES.small,gap:SIZES.small}}>
+                            <MotiText style={{fontSize:25,color:COLORS.white,fontWeight:600}}>NEW</MotiText>
+                                <MotiView style={{width:10,height:10,borderRadius:10,backgroundColor:COLORS.white}}></MotiView>
+                            <MotiText style={{fontSize:25,color:COLORS.yellow,fontWeight:500}}>STORY</MotiText>
+                        </MotiView>
+                        <MotiView style={{flexDirection:"row",justifyContent:"center",alignItems:"center",padding:SIZES.small,gap:SIZES.small}}>
+                            <MotiText style={{fontSize:35,textAlign:"center",color:COLORS.white,fontWeight:800}}>SHAKA SANZAGAKONA</MotiText>
+                        </MotiView>
+                        <MotiView style={{flexDirection:"row",justifyContent:"center",alignItems:"center",padding:SIZES.small,gap:SIZES.small}}>
+                            <MotiText style={{fontSize:15,textAlign:"center",color:COLORS.lightGrey,fontWeight:400}}>1800 , african history Drama , hero , history , africa</MotiText>
+                        </MotiView>
+                        <MotiView style={{marginTop:35,marginHorizontal:65,flexDirection:"row",justifyContent:"center",alignItems:"center",padding:2,borderRadius:10,gap:5,backgroundColor:COLORS.red,opacity:0.8}}></MotiView>
+                    </MotiView>
+
+                    <Animated.FlatList 
+                        data={IMAGES}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        style={{marginVertical:40}}
+                        snapToInterval={180}
+                        decelerationRate="fast"
+                        onScroll={Animated.event(
+                            [
+                                {
+                                    nativeEvent:{contentOffset:{x:position}}
+                                }
+                            ],
+                            {useNativeDriver:true}
+                        )}
+                        renderItem={({item})=>{
+                            console.log(item)
+                            return (
+                                <Card id={item.id} image={item.src} key={item.id} name={item.name} />
+                            )
+                        }}
+                    />
+                </ScrollView>
             </ImageBackground>
         </MotiSafeAreaView>
     
